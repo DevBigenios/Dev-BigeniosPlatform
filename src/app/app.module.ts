e41465e559d90338a8512ec1ae01ba2478f90092
Gabriela Barrezueta <gabrielabarrezueta94@gmail.com>
2022-12-05 17:19:03 -0500

    Se agrega componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { TestComponentComponent } from './test-component/test-component.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { RegisterComponent } from './register/register.component';
@@ -10,7 +9,6 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     TestComponentComponent,
     LogInComponent,
     RegisterComponent
